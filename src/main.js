f28de65152eee1f824e1d5616014903a51faeb06
owentangjintanakarn <owen.t95@hotmail.com>
2021-06-01 16:13:08 +0700

    Hosted

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import './plugins/bootstrap-vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 
+//Vue.prototype.$gmaps = window.google.maps
 Vue.config.productionTip = false
 Vue.use(Vuex)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
